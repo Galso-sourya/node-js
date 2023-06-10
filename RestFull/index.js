8eb5367d384a03fc0ffcd4f5f9188c1a3659d814
Galso-sourya <75131149+Galso-sourya@users.noreply.github.com>
2023-06-10 11:36:23 +0530

    Update index.js

--- RestFull/index.js
+++ RestFull/index.js
@@ -11,7 +11,7 @@ app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
 //as we do not have database, we are using array
-const comments=[
+let comments=[
     {id:uuid(),user:"kartik",text:"dnjdnd"},
     {id:uuid(),user:"ganesh",text:"cjdncjdnc"},
 ]
